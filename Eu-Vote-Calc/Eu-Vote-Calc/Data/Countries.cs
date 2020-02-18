fe85511be448d1b2ee7457f513de1a4442263631
err0rz-404 <60301479+err0rz-404@users.noreply.github.com>
2020-02-18 10:18:04 +0000

    v 0.0.2
    Classes added

---
+++ Eu-Vote-Calc/Eu-Vote-Calc/Data/Countries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eu_Vote_Calc.Data
+{
+    class Countries
+    {
+        public string name;
+        public bool Part;
+        public int Vote;
+        public int population;
+    }
+}
